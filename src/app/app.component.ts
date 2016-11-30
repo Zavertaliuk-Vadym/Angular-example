d37cedb6a036fd763932ce2513dd766a52886d8f
employee <vadymz@softwareplanet.uk.com>
2016-11-30 18:04:14 +0200

    rename title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +9,7 @@ export class AppComponent {
   title = 'Tour of Heroes';
   heroes = HEROES;
   selectedHero: Hero;
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
@@ -18,15 +19,15 @@ export class Hero {
   name: string;
 }
 const HEROES: Hero[] = [
-  { id: 1, name: 'Mr. Vadimka' },
-  { id: 11, name: 'Mr. Tomcat' },
-  { id: 12, name: 'Mr. Herocu' },
-  { id: 13, name: 'Ms. Java' },
-  { id: 14, name: 'Ms. Spring' },
-  { id: 15, name: 'Mr. Hibernate' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  {id: 1, name: 'Mr. Vadimka'},
+  {id: 11, name: 'Mr. Tomcat'},
+  {id: 12, name: 'Mr. Herocu'},
+  {id: 13, name: 'Ms. Java'},
+  {id: 14, name: 'Ms. Spring'},
+  {id: 15, name: 'Mr. Hibernate'},
+  {id: 16, name: 'RubberMan'},
+  {id: 17, name: 'Dynama'},
+  {id: 18, name: 'Dr IQ'},
+  {id: 19, name: 'Magma'},
+  {id: 20, name: 'Tornado'}
 ];
