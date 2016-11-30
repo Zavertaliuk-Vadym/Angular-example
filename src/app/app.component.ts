259e1bde7dca4e91a883dfec54a796786e085100
employee <vadymz@softwareplanet.uk.com>
2016-11-30 15:52:45 +0200

    add output and input name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <h1>{{title}}</h1>
+  <h2>{{hero.name}} fact!</h2>
+  <div><label>id: </label>{{hero.id}}</div>
+  <div>
+    <label>name: </label>
+    <input [(ngModel)]="hero.name" placeholder="name">
+  </div>
+
+  `
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Tour of Heroes';
+  hero: Hero = {
+    id: 1,
+    name: 'Vadimka'
+  };
+}
+export class Hero {
+  id: number;
+  name: string;
 }
