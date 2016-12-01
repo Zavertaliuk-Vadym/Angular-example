db1d9d8e64d4bcfa466633dabed6b36731028934
employee <vadymz@softwareplanet.uk.com>
2016-12-01 13:05:31 +0200

    finish part 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,28 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Hero} from "./hero-detail/hero";
+import {HEROES} from "./mock-heroes";
+import {HeroService} from "./hero.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[HeroService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  ngOnInit(): void {
+    this.getHeroes();
+  }
   title = 'Tour of Heroes';
-  heroes = HEROES;
-  hero: Hero;
+  heroes: Hero[];
   selectedHero: Hero;
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
+  constructor(private heroService:HeroService){
+  }
+  getHeroes():void {
+    this.heroService.getHeroesSlowly().then(heroes=>this.heroes=heroes);
+  }
 }
-const HEROES: Hero[] = [
-  {id: 1, name: 'Mr. Vadimka'},
-  {id: 11, name: 'Mr. Tomcat'},
-  {id: 12, name: 'Mr. Herocu'},
-  {id: 13, name: 'Ms. Java'},
-  {id: 14, name: 'Ms. Spring'},
-  {id: 15, name: 'Mr. Hibernate'},
-  {id: 16, name: 'RubberMan'},
-  {id: 17, name: 'Dynama'},
-  {id: 18, name: 'Dr IQ'},
-  {id: 19, name: 'Magma'},
-  {id: 20, name: 'Tornado'}
-];
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import {Hero} from './hero-detail/hero';
+
+export const HEROES: Hero[] = [
+  {id: 1, name: 'Mr. Vadimka'},
+  {id: 11, name: 'Mr. Tomcat'},
+  {id: 12, name: 'Mr. Herocu'},
+  {id: 13, name: 'Ms. Java'},
+  {id: 14, name: 'Ms. Spring'},
+  {id: 15, name: 'Mr. Hibernate'},
+  {id: 16, name: 'RubberMan'},
+  {id: 17, name: 'Dinama'},
+  {id: 18, name: 'Dr IQ'},
+  {id: 19, name: 'Magma'},
+  {id: 20, name: 'Tornado'}
+];
